4d921ae577a15ae361a9ec021601ada52d8d7c59
ibykov <igor.bykov@booking.com>
2018-10-15 15:37:51 +0200

    Fix gcloud

--- app/src/main/java/com/bykov/igor/githubstars/data/network/GithubService.kt
+++ app/src/main/java/com/bykov/igor/githubstars/data/network/GithubService.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 
 interface GithubService {
 
-  @GET("search/users?q=followers:%3E1000+sort:followers&per_page=3")
+  @GET("search/users?q=followers:%3E1000+sort:followers&per_page=3&access_token=bd300adb1382bcb642eb64c468544241c27bab16")
   fun getUser(): Deferred<GithubRepsonse>
 }
